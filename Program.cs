8a65f2458aa1e100716cf3598f82a8937b8b7fb4
Gyuris Csaba <gycsaba@vasvari.hu>
2018-11-22 08:07:52 +0100

    Adatbazis.cs, Prgoram.cs és MySQLDatabaseInterface.cs forráskódok hozzáadva a projekthez.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectToMysqlDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///<summary>#01 Adatbázis adatok pélányosítása.</summary>
+            Adatbazis a = new Adatbazis();
+            ///<summary>#01 MySQLDatabaseInterfece objektum létrehozása az adatábzis kapcsolat adataival.</summary>
+            MySQLDatabaseInterface mdi = a.kapcsolodas();
+
+            try
+            {
+                ///<summary>#01 Kapcsolat nyitása az adatbázishoz.</summary>                
+                mdi.open();
+                Console.WriteLine("Sikeres kapcsolódás az adatbázishoz...");
+                ///<summary>#02 Kapcsolat bezárása.</summary>                
+                mdi.close();
+            }
+            catch (Exception e)
+            {
+                ///<summary>#01 Sikertelen adatbázis művelet esetén kivétel kezelés.</summary>                
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
